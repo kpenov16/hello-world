42cb2030176b665c0b5fa62da243c1007fd348d4
Kaloyan Penov <s133967@student.dtu.dk>
2018-12-21 20:15:30 +0100

    - refactor givenTwoNumbersSeparatedByComma_returnNumbersSum: from apply to reduce

--- src/hello_world/core.clj
+++ src/hello_world/core.clj
@@ -1,7 +1,7 @@
 (ns hello-world.core)
 
 (defn sum-the-string [string]
-      (if (empty? string) 0 (apply + (map read-string (clojure.string/split string #","))))
+      (if (empty? string) 0 (reduce + (map read-string (clojure.string/split string #","))))
 
   )
 
